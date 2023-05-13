a67394b67fbbb29e116c5a0ef499b13874e11834
QtaroSPOH <131532380+QtaroSPOH@users.noreply.github.com>
2023-05-13 19:27:47 +0600

    Add files via upload

---
+++ Teleport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportPoint;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        collision.transform.position = teleportPoint.position;
+    }
+}
