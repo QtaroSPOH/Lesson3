2e240f7ca3518bd4da96f01fd57ead4e64136668
QtaroSPOH <131532380+QtaroSPOH@users.noreply.github.com>
2023-05-13 19:28:35 +0600

    Add files via upload

---
+++ JumpTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    [Range(0, 100)]
+    public float stregnthFactor = 4.3f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength *= stregnthFactor;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<Jump>().jumpStrength /= stregnthFactor;
+    }
+}
