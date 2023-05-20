5a68a3992a7528c1e52f4a8d5ca047f79978a56f
QtaroSPOH <131532380+QtaroSPOH@users.noreply.github.com>
2023-05-20 19:44:59 +0600

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int coins;
+    public AudioSource audiosource;
+    public AudioClip damagedSound;
+    private int health = 10;
+
+    public void CollectCoins()
+    {
+        coins++;
+
+// audiosource.PlayOneShot(collectSound);
+
+        print("Собранные монетки:" + coins);
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока:" + health);
+    }
+
+    
+
+}
