f496a6b3ac9f5214754a886c617ac9b253e81984
QtaroSPOH <131532380+QtaroSPOH@users.noreply.github.com>
2023-06-16 12:11:42 +0300

    Add files via upload

---
+++ Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Animator button;
+    public Animator door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.enabled = false;
+        door.enabled = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        button.enabled = true;
+        door.enabled = true;
+    }
+}
