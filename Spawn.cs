ce505ce53c46ad20a2722a6d240540641c289108
QtaroSPOH <131532380+QtaroSPOH@users.noreply.github.com>
2023-06-22 18:08:16 +0300

    Update Spawn.cs

--- Spawn.cs
+++ Spawn.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject prefab;
+    
+    public Transform borderRight;
+    public Transform borderLeft;
+    
+    public float Intrerval;
+    public float Timer;
 
-    // Update is called once per frame
+    void Spawn()
+    {
+        float randomX = Random.Range(borderLeft.position.x, borderRight.position.x);
+        
+        Vector2 newPosition = transform.position;
+        newPosition.x = randomX;
+        
+        Instantiate(prefab, newPosition, Quaternion.identuty);
+        Timer = Interval;
+    }
+    
     void Update()
     {
-        
+       Timer -= Time.deltaTime;
+       
+       if(Timer <=0)
+       {
+            Spawn();
+       }
     }
 }
